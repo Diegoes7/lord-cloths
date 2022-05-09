844b5bed1123fd0c978657eb70f128f072d7147e
Diegoes7 <dimitar.i.dimitrov77@gmail.com>
2022-05-09 15:00:07 +0300

    adding pwa

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { stripePromise } from "./utils/stripe/stripe.utils";
 import App from "./App";
 // import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorkerRegistration";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -31,3 +33,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.register()
